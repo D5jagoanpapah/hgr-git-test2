8ca7d1a7876d69d5c35418c6b0ae3931c2485493
delimanis <hegarisya123@gmail.com>
2024-01-10 13:38:30 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,12 @@
 <body>
         <h1>halooo</h1>
         <p>10/01/2024</p>
+
+
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
 </body>
-</html>
+</html> 
